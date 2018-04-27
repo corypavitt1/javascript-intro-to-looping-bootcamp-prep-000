e7f68a0369a28bd8ae24244ddde0b6b0d7389e27
Cory Pavitt <corypavitt@hotmail.com>
2018-04-27 17:30:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (var i = 0; i < array.length; i++) {
-    if (i === 1) {
+    if (i === 2) {
       array.push(`I am 1 strange loop.`)
     } else {array.push(`I am ${i} strange loops.`)}
   }
